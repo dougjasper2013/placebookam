ac5a446c27fd3e94ddefeb93f6d9cf5976291fe1
Douglas Jasper <doug.jasper@trios.com>
2024-08-26 10:59:27 -0400

    Added Place Photo

--- placebook/app/src/main/java/com/trios2024evdj/placebook/MapsActivity.kt
+++ placebook/app/src/main/java/com/trios2024evdj/placebook/MapsActivity.kt
@@ -7,6 +7,7 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.common.api.ApiException
@@ -18,6 +19,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PointOfInterest
@@ -25,6 +27,7 @@ import com.trios2024evdj.placebook.databinding.ActivityMapsBinding
 
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FetchPhotoRequest
 import com.google.android.libraries.places.api.net.FetchPlaceRequest
 import com.google.android.libraries.places.api.net.PlacesClient
 
@@ -56,6 +59,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun displayPoi(pointOfInterest: PointOfInterest) {
+        displayPoiGetPlaceStep(pointOfInterest)
+    }
+
+    private fun displayPoiGetPlaceStep(pointOfInterest: PointOfInterest) {
         val placeId = pointOfInterest.placeId
 
         val placeFields = listOf(Place.Field.ID,
@@ -72,21 +79,74 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         placesClient.fetchPlace(request)
             .addOnSuccessListener { response ->
                 val place = response.place
-                Toast.makeText(this,
-                    "${place.name}, " +
-                "${place.phoneNumber}",
-                    Toast.LENGTH_LONG).show()
+                displayPoiGetPhotoStep(place)
+//                Toast.makeText(this,
+//                    "${place.name}, " +
+//                            "${place.phoneNumber}",
+//                    Toast.LENGTH_LONG).show()
             }.addOnFailureListener {
-                exception ->
+                    exception ->
                 if (exception is ApiException) {
                     val statusCode = exception.statusCode
                     Log.e(TAG,
                         "Place not found: " +
-                        exception.message + ", " +
-                        "statusCode: " + statusCode)
+                                exception.message + ", " +
+                                "statusCode: " + statusCode)
                 }
             }
     }
+
+    private fun displayPoiGetPhotoStep(place: Place) {
+        // 1
+        val photoMetadata = place
+            .getPhotoMetadatas()?.get(0)
+        // 2
+        if (photoMetadata == null) {
+            // Next step here
+            displayPoiDisplayStep(place, null)
+            return
+        }
+        // 3
+        val photoRequest = FetchPhotoRequest
+            .builder(photoMetadata)
+            .setMaxWidth(resources.getDimensionPixelSize(
+                R.dimen.default_image_width))
+            .setMaxHeight(resources.getDimensionPixelSize(
+                R.dimen.default_image_height))
+            .build()
+        // 4
+        placesClient.fetchPhoto(photoRequest)
+            .addOnSuccessListener { fetchPhotoResponse ->
+                val bitmap = fetchPhotoResponse.bitmap
+                // Next step here
+                displayPoiDisplayStep(place, bitmap)
+            }.addOnFailureListener { exception ->
+                if (exception is ApiException) {
+                    val statusCode = exception.statusCode
+                    Log.e(TAG,
+                        "Place not found: " +
+                                exception.message + ", " +
+                                "statusCode: " + statusCode)
+                }
+            }
+    }
+
+    private fun displayPoiDisplayStep(place: Place, photo: Bitmap?) {
+        val iconPhoto = if (photo == null) {
+            BitmapDescriptorFactory.defaultMarker()
+        } else {
+            BitmapDescriptorFactory.fromBitmap(photo)
+        }
+
+        mMap.addMarker(MarkerOptions()
+            .position(place.latLng as LatLng)
+            .icon(iconPhoto)
+            .title(place.name)
+            .snippet(place.phoneNumber)
+        )
+    }
+
+
 
     /**
      * Manipulates the map once available.
